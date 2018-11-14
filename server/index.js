9d6f739592589bbd68d6879f5916f7bd40513265
xmq291 <xmq291@gmail.com>
2018-11-14 16:48:00 -0500

    Serving static files

--- server/index.js
+++ server/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 
 const routes = require('./routes');
-
+app.use(express.static('public'));
+app.get('/favicon.ico', (req, res, next) => {
+    return res.sendStatus(204);
+})
 app.use('/', routes());
 
 app.listen(3000);
