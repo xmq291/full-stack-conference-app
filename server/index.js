f5680bd9235e9995cfc187c67117b452f82d7b84
xmq291 <xmq291@gmail.com>
2018-11-15 21:40:29 -0500

    Creating a custom error page

--- server/index.js
+++ server/index.js
@@ -1,20 +1,35 @@
 const express = require('express');
+const createError = require('http-errors');
 const path = require('path');
+
 const app = express();
 
 app.set('view engine', 'pug');
-if (app.get('env') === 'development') {
+if(app.get('env') === 'development') {
     app.locals.pretty = true;
 }
-
 app.set('views', path.join(__dirname, './views'));
 
 const routes = require('./routes');
 app.use(express.static('public'));
 app.get('/favicon.ico', (req, res, next) => {
     return res.sendStatus(204);
-})
+});
+
 app.use('/', routes());
+
+app.use((req, res, next) => {
+    return next(createError(404, 'File not found'));
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    const status = err.status || 500;
+    res.locals.status = status;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(status);
+    return res.render('error');
+});
 
 app.listen(3000);
 
