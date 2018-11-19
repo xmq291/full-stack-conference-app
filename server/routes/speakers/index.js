7e720985210b55cc6dff9ba2563c3aec99a4b4bd
xmq291 <xmq291@gmail.com>
2018-11-18 23:02:25 -0500

    Creating reusable components with layouts and blocks

--- server/routes/speakers/index.js
+++ server/routes/speakers/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 module.exports = () => {
     router.get('/', (req, res, next) => {
-        return res.send('All Speakers');
+        return res.render('speakers');
     });
 
     router.get('/:name', (req, res, next) => {
