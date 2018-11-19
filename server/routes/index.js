99a95355dc0fa7a0d509a717ecfe711baab07959
xmq291 <xmq291@gmail.com>
2018-11-18 23:39:23 -0500

    Adding Central Configuration

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,18 +1,38 @@
 const express = require('express');
+const createError = require('http-errors');
+const path = require('path');
+const configs = require('./config');
+const app = express();
 
-const router = express.Router();
+const config = configs[app.get('env')];
 
-const speakerRoute = require('./speakers');
+app.set('view engine', 'pug');
+if(app.get('env') === 'development') {
+    app.locals.pretty = true;
+}
+app.set('views', path.join(__dirname, './views'));
 
-const feedbackRoute = require('./feedback');
+const routes = require('./routes');
+app.use(express.static('public'));
+app.get('/favicon.ico', (req, res, next) => {
+    return res.sendStatus(204);
+});
 
-module.exports = () => {
-    router.get('/', (req, res, next) => {
-        return res.render('index');
-    });
+app.use('/', routes());
 
-    router.use('/speakers', speakerRoute());
-    router.use('/feedback', feedbackRoute());
+app.use((req, res, next) => {
+    return next(createError(404, 'File not found'));
+});
 
-    return router;
-};
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    const status = err.status || 500;
+    res.locals.status = status;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(status);
+    return res.render('error');
+});
+
+app.listen(3000);
+
+module.export = app;
